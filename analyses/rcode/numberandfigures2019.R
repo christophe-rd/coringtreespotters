a513cb3955800aebce58d760c3a21ad82b427407
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-03-04 19:28:09 -0800

    commiting to ignore the huge file from the arboretum

--- analyses/rcode/numberandfigures2019.R
+++ analyses/rcode/numberandfigures2019.R
@@ -1,74 +1,48 @@
 ### Time to visualize the numbers
 # 29 July 2019 - Cat
-
-#change by crd to see if it works in November 2024
+# November 2023 updated by CRD 
 
 # Clear workspace
 rm(list=ls()) # remove everything currently held in the R memory
 options(stringsAsFactors=FALSE)
-
-
 # Load libraries
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(RColorBrewer)
-
 library(viridis)
 
 # Set Working Directory
-setwd("/Users/christophe_rouleau-desrochers/github/coringtreespotters/analyses/input/")
+setwd("/Users/christophe_rouleau-desrochers/github/coringtreespotters/analyses/")
 list.files()
 
-b<-read.csv("individual_phenometrics_data.csv", header=TRUE)
-head(b)
-# read approved list of trees to core and remove non necessary columns
-dcore <- read.csv("2025ApprovedPlantListforcoring.csv")[1:5]
-head(dcore)
-# read csv that contains the two tree names and nicknames
-namemerge <- read.csv2("treeswithPhenodata.csv", header=TRUE, sep=",")
-
-# rename columns so they are more consistent
-colnames(dcore) <- c("availableToCore2025", "previouslyCored", "yearCored", "Plant_Nickname", "species")
-dcore <- subset(dcore, availableToCore2025 == "Y")
-nrow(dcore)
-# Split individual Id and QUAL
-dcore$plantNickname <- sub("\\*.*", "", dcore$Plant_Nickname)
-namemerge$plantNickname <- sub(",.*", "", namemerge$Plant_Nickname)
-# replace manually the American beech
-namemerge$plantNickname[which(namemerge$Plant_Nickname =="14585*J American beech")] <- "14585*J"
-namemerge$plantNickname[which(namemerge$Plant_Nickname =="22798*A American beech")] <- "22798*A"
-# add nickname column since it's the one the arboretum uses
-b$plantNickname <- namemerge$plantNickname[match(b$Individual_ID, namemerge$Individual_ID)]
-
-### check for how many years of red maple we have
-notasked2core <- unique(b$Individual_ID)
-asked2core <- unique(namemerge$Individual_ID)
-m <- c(notasked2core, asked2core)
-m[!duplicated(m)]
-unique_values <- m[!duplicated(m) & !duplicated(m, fromLast = TRUE)]
-subset_df <- b[b$Individual_ID %in% unique_values, ]
-redmaple <- subset(b, Common_Name == "red maple")
-nrow(redmaple)
-unique(redmaple$First_Yes_Year)
-
-
-# b$plantNickname <- sub("\\*.*", "", b$Plant_Nickname)
-# create vector for all the individuals we can core
-id2core<-as.character(dcore$Plant_Nickname)
-###### Breakdown of observations per species
-b$spp <- paste(b$Genus, b$Species)
-numobsperspp <- b %>% count(spp)
+b<-read.csv("output/cleanedStatusIntensity.csv", header=TRUE)
+# === === === === === === === === === === === === === === ===
+# subset for red maples
+red <- subset(fulldf, Species == "rubrum")
+# nicknames for redmaples
+vec <- unique(red$Plant_Nickname)
+# subset back to all data for red maples
+redmaple <- subset(fulldf, Plant_Nickname %in% vec)
+# remove duplicates
+redmaple3 <- redmaple[!duplicated(redmaple$Plant_Nickname),]
+subby <- redmaple3[, c("Individual_ID", "Plant_Nickname", "GenusSpecies")]
+# write small csv for redmaples
+write.csv2(subby, "output/listRedMaples.csv")
+# === === === === === === === === === === === === === === ===
+# Breakdown of observations per species
+numobsperspp <- b %>% count(Common_Name)
 head(numobsperspp)
 #colz <- colorRampPalette(brewer.pal(15, "Dark2"))(15)
 colz <- viridis_pal(option="D")(15)
-colourCount = length(unique(numobsperspp$spp))
-getPalette = colorRampPalette(brewer.pal(8, "Dark2"))
+colourCount = length(unique(b$Common_Name))
+getPalette <- colorRampPalette(brewer.pal(8, "Dark2"))
 
-sppnum <- ggplot(numobsperspp, aes(x=spp, y=n, fill=spp)) + 
+
+sppnum <- ggplot(numobsperspp, aes(x=Common_Name, y=n, fill=Common_Name)) + 
   geom_bar(stat="identity", position=position_dodge()) +
   scale_fill_manual(values=getPalette(colourCount), 
-                    labels=numobsperspp$spp) + 
+                    labels=numobsperspp$Common_Name) + 
   labs(title="Total number of observed phenophases \n(e.g., leaves or flowers) for each species (2015-2019).") +
   scale_x_discrete(labels=c("Acer rubrum" = "Red Maple",
                             "Acer saccharum" = "Sugar Maple",
@@ -91,66 +65,55 @@ sppnum <- ggplot(numobsperspp, aes(x=spp, y=n, fill=spp)) +
                           plot.title = element_text(face="bold")) +
   ylab("Number of observed phenophases") + xlab("") +
   geom_text(aes(label=n), hjust=0.4, vjust=-1, col=getPalette(colourCount))
-
 sppnum
 
-
-
 # nb of observations per individuals for treespotters data
-obsperind <- b[!duplicated(b$Individual_ID),]
-str(obsperind)
-nbobsperID <- b %>% count(Common_Name, Individual_ID, Last_Yes_Year)
-nrow(nbobsperID)
-head(nbobsperID)
-nbobsperID <- nbobsperID[!is.na(nbobsperID$Individual_ID),]
-# set color palet
-colourCount <- length(unique(nbobsperID$Common_Name))
-getPalette <- colorRampPalette(brewer.pal(9, "Set1")) 
-nbobsperID$Individual_ID <- as.character(nbobsperID$Individual_ID)
-# PLOT ok for now i guess
-ggplot(nbobsperID, aes(x = n, y = factor(Individual_ID), fill = factor(Last_Yes_Year))) +
+nbobsperID <- b %>% count(Common_Name, Observation_ID, year)
+# reorder years so 2015 is on the left
+nbobsperID$year <- factor(nbobsperID$year, levels = 2020:2015) 
+fall_palette <- c("#B22222", "#D2691E", "#FF8C00", "#FFA500", "#FFD700", "#8B4513")
+# Plot by species and years
+countperspp <- ggplot(nbobsperID, aes(x = n, y = factor(Common_Name), fill = factor(year))) +
   geom_bar(stat = "identity", position = "stack") +  # Stack years within each bar
-  labs(x = "Count", y = "Individual ID", fill = "Year") +
+  labs(x = "Number of phenological observations", y = "Species", fill = "Year") +
   theme_classic() + 
   theme(legend.position = "right", 
         plot.margin = unit(c(1, 3, 1, 1), "lines"),
         plot.title = element_text(face = "bold")) +
-  scale_fill_manual(values = getPalette(length(unique(nbobsperID$Last_Yes_Year))))  # Adjust palette for years
-
-
-vec <- c("sugar maple", "red maple")
-redandsugar <- nbobsperID[nbobsperID$Common_Name %in% vec, ]
-test <- redandsugar[!is.na(redandsugar$Last_Yes_Year),]
-
-# Define the color palette
-getPalette2 <- colorRampPalette(brewer.pal(3, "Dark2"))
-
-# Ensure Last_Yes_Year is an ordered factor with 2015 at the bottom and 2020 at the top
-test$Last_Yes_Year <- factor(test$Last_Yes_Year, levels = 2020:2015)  # Reverse order for stacking
-
+  scale_fill_manual(values=fall_palette)
+countperspp
+ggsave("figures/countPerSpecies.pdf", countperspp, bg = "white", width =  10, height = 10,)
+# vec <- c("sugar maple", "red maple")
+# redandsugar <- nbobsperID[nbobsperID$Common_Name %in% vec, ]
+# vec <- c("red maple")
+# redandsugar <- nbobsperID[b$plantNickname %in% vec, ]
+# test <- redandsugar[!is.na(redandsugar$Last_Yes_Year),]
+# # Define the color palette
+# getPalette2 <- colorRampPalette(brewer.pal(3, "Dark2"))
+# 
+# # Ensure Last_Yes_Year is an ordered factor with 2015 at the bottom and 2020 at the top
+# test$Last_Yes_Year <- factor(test$Last_Yes_Year, levels = 2020:2015)  # Reverse order for stacking
 # Plot with years ordered correctly in the stacked bars
 # Ensure Last_Yes_Year is an ordered factor with 2015 at the bottom and 2020 at the top
-redandsugar$Last_Yes_Year <- factor(redandsugar$Last_Yes_Year, levels = 2020:2015)  # Reverse order for stacking
+# redandsugar$Last_Yes_Year <- factor(redandsugar$Last_Yes_Year, levels = 2020:2015)  # Reverse order for stacking
 
-# Plot for sugar and red maples
-ggplot(redandsugar, aes(x = factor(Individual_ID), y = n, fill = Last_Yes_Year)) +
-  geom_bar(stat = "identity", position = "stack") +
-  facet_wrap(~ Common_Name, ncol = 3, scales = "free_x") +  # Use free_x to allow different x-axis scales
-  labs(x = "Individual ID", y = "Count", fill = "Years") +
-  theme_classic() +
-  theme(legend.position = "right", 
-        plot.margin = unit(c(1, 3, 1, 1), "lines"),
-        plot.title = element_text(face = "bold")) +
-  scale_fill_manual(values = getPalette2(length(unique(redandsugar$Last_Yes_Year))))
+# # Plot for sugar and red maples
+# graph <- ggplot(redandsugar, aes(x = factor(Individual_ID), y = n, fill = Last_Yes_Year)) +
+#   geom_bar(stat = "identity", position = "stack") +
+#   facet_wrap(~ Common_Name, ncol = 3, scales = "free_x") +  # Use free_x to allow different x-axis scales
+#   labs(x = "Individual ID", y = "Count", fill = "Years") +
+#   theme_classic() +
+#   theme(legend.position = "right", 
+#         plot.margin = unit(c(1, 3, 1, 1), "lines"),
+#         plot.title = element_text(face = "bold")) +
+#   scale_fill_manual(values = getPalette2(length(unique(redandsugar$Last_Yes_Year))))
+# list.files()
+# ggsave("output/redsugar.pdf", graph)
 
-# nb of observations per individuals for treespotters data AND allowed trees to core
-### Subset for trees we can core
-
-filtered_dcore <- b[b$plantNickname %in% id2core, ]
-nbobsperID <- filtered_dcore %>% count(Common_Name, plantNickname, Last_Yes_Year)
+# number of observations per tree
+nbobsperID <- b %>% count(Common_Name, plantNickname, year)
 nbobsperID <- nbobsperID[!is.na(nbobsperID$plantNickname),]
-
-ggplot(nbobsperID, aes(x = n, y = reorder(plantNickname, n), fill = Common_Name)) +
+obspertree <- ggplot(nbobsperID, aes(x = n, y = reorder(plantNickname, n), fill = Common_Name)) +
   geom_bar(stat = "identity") +
   labs(x = "Count", y = "Individual ID") +
   theme_classic() + 
@@ -158,21 +121,22 @@ ggplot(nbobsperID, aes(x = n, y = reorder(plantNickname, n), fill = Common_Name)
         plot.margin = unit(c(1, 3, 1, 1), "lines"),
         plot.title = element_text(face = "bold")) +
   scale_fill_manual(values = getPalette(colourCount))
+ggsave("figures/observationPerTree.pdf", obspertree)
 
 ### Now let's make a plot with time-series data
 # Observations per year
-numobsperyr <- b %>% count(Last_Yes_Year)
-numobsperyr
+numobsperyr <- b %>% count(year)
+
 cumulative <- numobsperyr %>%
   mutate(cumulative_sum = cumsum(n))
 cols<-colorRampPalette(brewer.pal(8, "Dark2"))(5)
 colpal <- colorRampPalette(c("blue3", "red3"))
 colvir <- viridis_pal(option="C")(6)
-ggplot(cumulative, aes(x=Last_Yes_Year, y=cumulative_sum)) + 
+ggplot(cumulative, aes(x=year, y=cumulative_sum)) + 
   geom_line() + 
-  geom_point(aes(col=as.factor(Last_Yes_Year))) + 
+  geom_point(aes(col=as.factor(year))) + 
   theme_classic() + 
-  scale_color_manual(values=colpal(6), labels=cumulative$Last_Yes_Year) + 
+  scale_color_manual(values=colpal(6), labels=cumulative$year) + 
   theme(legend.position = "none",
         plot.margin = unit(c(1,4.5,1,1), "lines"),
         plot.title=element_text(face="bold"),
